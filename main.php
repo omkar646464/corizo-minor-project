863e6caf14bf66d8dbb002018897dd3efe9054af
omkar646464 <163818523+omkar646464@users.noreply.github.com>
2024-03-18 12:53:14 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,324 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+#one{
+  width:50px;
+  height:100px;
+  color:crimson;
+  font-size: 30px;
+  text-align: center;
+}
+#two{
+  cursor :pointer;
+}
+#three{
+  background :white;
+}
+img{
+  margin:5px;
+}
+#text{
+  text-align:justify;
+}
+input[type=text],select{
+  width:100%;
+  padding:12px 20px;
+  margin:8px 0;
+  display:inline-block;
+  border :1px solid #ccc;
+  border-radius:4px;
+  box-sizing:border-box;
+}
+input[type=submit]{
+  width:100%;
+  color:white;
+  background :#4cAF50;
+  padding:14px 20px;
+  margin:8px 0;
+  border :none;
+  border-radius:4px;
+  cursor:pointer;
+}
+input[type=submit]:hover{
+  background :#45a049;
+}
+#form{
+  border-radius:5px;
+  background :#2f2f2;
+  padding:20px;
+}
+@media screen and (max-width:600px){
+  .container{
+    width:100%;
+  }
+  .container-sm{
+    width:100%;
+  }
+  *{
+    width:100%;
+  }
+}
+</style>
+</head>
+
+<body>
+  <!-------heading------------>
+  <div class="container mt-4">
+ <h1>GetMore</h1>
+ <p>One stop for everything</p>
+ </div>
+
+ <!---------second heading--------->
+<div class="container p-3 mt-4">
+<div class="row">
+<div class="col" id="one">Explore the new shopping experience </div>
+</div>
+
+<!-----------navigation----------->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+  <a href="#" class="navbar-brand">GetMore</a>
+ <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarcollapse">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbar-collapse">
+<div class="navbar-nav mx-5">
+<a href="#one" class="nav-item nav-link active">CLOTHING</a>
+<a href="#four" class="nav-item nav-link ">GROCERIES</a>
+<a href="#skin" class="nav-item nav-link ">ELECTRONICS</a>
+<a href="#foot" class="nav-item nav-link ">FOOT WEAR</a>
+</div>
+<div class="navbar-nav ms-auto">
+  <a href="" class="nav-item nav-link">Login</a>
+  <a href="" class="nav-item nav-link">Signup</a>
+  </div>
+  </div>
+  </div>
+  </nav>
+  <br><br><br>
+
+  <!---------clothing--------------->
+  <div class="container p-3 mt-4">
+  <div class="row">
+ <div class="col" id="one">CLOTHING</div>
+ </div>
+ <div class="container mx-4" id="three">
+ <div class="row">
+ <div class="col-sm-4"><img src="https://m.media-amazon.com/images/I/71MOhqtTvWL._AC_UY1100_.jpg" width="200px" height="200px">
+ <p>Rs.799/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+ <div class="col-sm-4"><img src="https://rukminim2.flixcart.com/image/850/1000/xif0q/shirt/j/z/s/xl-nemhs5-nem-original-imagrfsyfkfdhcmu.jpeg?q=20&crop=false" width="200px" height="200px">
+   <p>Rs.899/-</p>
+   <p><a href="" class="btn btn-success">BUY NOW</a></p>
+ </div>
+ <div class="col-sm-4"><img src="https://www.louisphilippe.com/blog/wp-content/uploads/2022/07/Select-The-Best-From-This-Stellar-Range-Of-Mens-T-shirts-Louis-Philippe-Fashion-Blogs.jpg" width="200px" height="200px"><br><br>
+  <p>Rs.699/-</p>
+  <p><a href="" class="btn btn-success">BUY NOW</a></p>
+ </div>
+ </div>
+ <br>
+ <div class="row">
+ <div class="col-sm-4"><img src="https://img0.junaroad.com/uiproducts/20319982/zoom_0-1695314983.jpg" width="200px" height="200px">
+<br><br>
+ <p>Rs.999</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+ </div>
+ <div class="col-sm-4"><img src="https://www.nevaindia.com/cdn/shop/products/3H8A8498_grande.jpg?v=1663241516" width="200px" height="200px">
+ <br><br>
+ <p>Rs.1199</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>  
+<div class="col-sm-4"><img src="https://5.imimg.com/data5/KR/PL/MY-47682622/mens-multicolor-pant-500x500.jpg" width="200px" height="200px">
+ <br><br>
+ <p>Rs.1299</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+  </div>
+ </div>
+ <br><br>
+    <!-------------GROCERIES--------------->
+ <div class="container-sm p-3 mt-4">
+ <div class="row">
+ <div class="col" id="one">GROCERIES</div>
+ </div>
+<div class="container-sm mx-4" id="four">
+ <div class="row">
+ <div class="col-sm"><img src="https://thumbs.dreamstime.com/b/shopping-basket-groceries-isolated-over-white-background-34518038.jpg" width="200px" height="200px"><br><br>
+<p>Rs.149/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+ <div class="col-sm"><img src="https://static.blog.bolt.eu/LIVE/wp-content/uploads/2022/04/30135418/grocery-list.jpg" width="200px" height="200px"><br><br>
+<p>Rs.99/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+ </div>
+ <div class="col-sm"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJd7eXyidrDGmkqm-RR2F3VyCa1j-gIc9ytToOxYcUHw&s" width="200px" height="200px"><br><br>
+  <p>Rs.119/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+ </div>
+ </div>
+ <br>
+ <div class="row">
+<div class="col-sm-4"><img src="https://akm-img-a-in.tosshub.com/businesstoday/images/story/201308/online-grocery-shopping_505_072413045216.jpg?size=948:533" width="200px" height="200px"><br><br>
+<p>Rs.199/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+<div class="col-sm-4"><img src="https://www.homelane.com/blog/wp-content/uploads/2021/03/shutterstock_1681776775-1.jpg" width="200px" height="200px"><br><br>
+<p>Rs.79/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+ <div class="col-sm-4"><img src="https://assets.bonappetit.com/photos/57d6f59fc204478524d874a8/16:9/w_2560%2Cc_limit/13065314334_a2a3178590_k.jpg" width="200px" height="200px"><br><br>
+<p>Rs.49/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+</div>
+</div>
+<br><br><br><br><br>
+
+ <!-------------ELECTRONICS--------------->
+ <div class="container-sm p-3 mt-4">
+ <div class="row">
+ <div class="col" id="one">ELECTRONICS</div>
+ </div>
+<div class="container-sm mx-4" id="skin">
+ <div class="row">
+ <div class="col-sm"><img src="https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$" width="200px" height="200px"><br><br>
+<p>Rs.11,999/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+ <div class="col-sm"><img src="https://cdn1.smartprix.com/rx-iVyp4t2Vm-w1200-h1200/Vyp4t2Vm.jpg" width="200px" height="200px"><br><br>
+<p>Rs.89,999/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+ </div>
+ <div class="col-sm"><img src="https://imgeng.jagran.com/images/2023/oct/Best%2013%20Generation%20Laptops%20In%20India1698484193529.jpg" width="200px" height="200px"><br><br>
+  <p>Rs.49,999/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+ </div>
+ </div>
+ <br>
+ <div class="row">
+<div class="col-sm-4"><img src="https://images.samsung.com/is/image/samsung/p6pim/in/mc28h5025vr-tl/gallery/in-28-litre-convection-microwave-oven-mc28h5025vk-274424-mc28h5025vr-tl-530434682?$650_519_PNG$" width="200px" height="200px"><br><br>
+<p>Rs.699/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+<div class="col-sm-4"><img src="https://media.idownloadblog.com/wp-content/uploads/2023/09/Various-types-of-Bluetooth-headsets.jpg" width="200px" height="200px"><br><br>
+<p>Rs.5,999/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+ <div class="col-sm-4"><img src="https://d3juy0zp6vqec8.cloudfront.net/images/product/1583929517Washing-Machine-Front-Load.webp" width="300px" height="200px"><br><br>
+<p>Rs.17,999/-</p>
+<p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+</div>
+</div>
+<br><br><br><br><br>
+<!------------About us-------------->
+<div class="container-sm mx-3">
+  <div class="row">
+  <div class="col-sm">
+  <h2>Our story </h2><br>
+  <p id="text"> A background on how the business got started and why, what it's known for, who is involved and anything that makes it stand out</p>
+ </div>
+ <div class="col-sm mt-5">
+ <h1><img src="https://get-more.be/wp-content/uploads/2021/12/logo-1.png" width="200px" height="200px"></h1>
+ </div>
+ </div>
+ </div>
+ 
+  <!------------Foot wear------------->
+  <div class="container-sm p-3 mt-4">
+ <div class="row">
+<div class="col" id="one">Foot wear</div>
+</div>
+<div class="container-sm mx-4" id="foot">
+ <div class="row" id="four">
+<div class="col-sm"><img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/18274390/2022/5/14/c64644f6-840f-4496-bb42-19359e9881771652523841987RoadsterMenBrownTexturedPUSneakers1.jpg" width="200px" height="200px"><br><br>
+ <p>Rs.699/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+<div class="col-sm"><img src="https://images.jdmagicbox.com/quickquotes/images_main/vkc-pride-mens-footwear-23-09-2020-047-210844609-l0l7h.jpg" width="200px" height="200px"><br><br>
+ <p>Rs.2,499/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+  <div class="col-sm"><img src="https://images.meesho.com/images/products/390266627/e1ktb_256.webp" width="200px" height="250px"><br><br>
+ <p>Rs.599/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+</div>
+<br>
+<div class="row">
+  <div class="col-sm"><img src="https://images.jdmagicbox.com/quickquotes/images_main/adidas-mens-outdoor-hoist-sandals-mens-footwear-carbon-sock-slime-core-black-size-11-160093902-necwt.jpg" width="200px" height="200px"><br><br>
+ <p>Rs.799/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+ <div class="col-sm"><img src="https://rukminim2.flixcart.com/image/850/1000/xif0q/sandal/e/z/e/-original-imagr6wkfrvqstex.jpeg?q=90&crop=false" width="200px" height="200px"><br><br>
+ <p>Rs.1,999/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+ <div class="col-sm"><img src="https://media.gq.com/photos/64a6e38586fb14a13de77295/master/pass/best-sandals-art.jpg" width="200px" height="200px"><br><br>
+ <p>Rs.3,499/-</p>
+ <p><a href="" class="btn btn-success">BUY NOW</a></p>
+</div>
+</div>
+</div>
+<br><br><br><br><br>
+<hr>
+<!------------Footer------------>
+<div class="container">
+<div class="row">
+<div class="col-sm-6">
+  <p>rights reserved</p>
+  </div>
+ <div class="col-sm-6 text-md-end">
+<a href="" class="text-dark">terms and conditions</a>
+<span class="text-muted mx-3">|</span>
+<a href="" class="text-dark">privacy</a>
+</div>
+</div>
+<br><br>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <a href="#" class="navbar-brand">GetMore</a>
+ <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarcollapse">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbar-collapse">
+<div class="navbar-nav mx-5">
+<a href="#one" class="nav-item nav-link active">Clothing</a>
+<a href="#four" class="nav-item nav-link ">GROCERIES</a>
+<a href="#skin" class="nav-item nav-link ">ELECTRONICS</a>
+<a href="#foot" class="nav-item nav-link ">FOOT WEAR</a>
+</div>
+<div class="navbar-nav ms-auto">
+  <a href="" class="nav-item nav-link">Login</a>
+  <a href="" class="nav-item nav-link">Signup</a>
+  </div>
+  </div>
+  </div>
+  </nav>
+  <br><br><br>
+  <p style="text-align: center">Join our community to know about latest arrivals and offers</p>
+  <form id="form">
+    <label for="fname">Name</label>
+    <input type="text" id="fname" name="first name" placeholder="your name">
+    <label for="lname">Mail ID</label>
+    <input type="text" id="lname" name="mail id" placeholder="your mail id">
+<label for="country">country</label>
+<select id="country" name="country">
+<option value="India">India</option>
+<option value="Canada">Canada</option>
+<option value="USA">USA</option>
+</select>
+<input type="submit" value="submit">
+</form>
+</div>
+</body>
+
+</html>
